a9df41ced0737c8e2d5d55eacadcb25e8ebaea5f
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-10-16 22:11:06 -0300

    Animations - idle and walk (+ camera follower)

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    private Vector3 locationOffset;
+    private Vector3 rotationOffset;
+
+    void Start()
+    {
+        locationOffset = transform.position - target.position;
+        rotationOffset = transform.eulerAngles - target.eulerAngles;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + locationOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        Quaternion desiredRotation = Quaternion.Euler(target.eulerAngles + rotationOffset);
+        Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+        transform.rotation = smoothedRotation;
+
+        transform.LookAt(target);
+    }
+}
