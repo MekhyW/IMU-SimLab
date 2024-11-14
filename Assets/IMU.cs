feb96fb598fe3651707983fffbccb0060a0ccd3d
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-11-14 14:07:19 -0300

    Use derivatives instead of rigidbody

--- Assets/IMU.cs
+++ Assets/IMU.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class IMUFollow : MonoBehaviour
@@ -8,25 +6,41 @@ public class IMUFollow : MonoBehaviour
     public float orientationX, orientationY, orientationZ;
     public float accelerationX, accelerationY, accelerationZ;
     public float gyroscopeX, gyroscopeY, gyroscopeZ;
-    private Rigidbody rb;
+    private Vector3 previousVelocity;
+    private Vector3 previousPosition;
+    private Quaternion previousRotation;
+    private float deltaTime;
 
     void Start()
     {
         transform.SetParent(target.transform);
-        rb = GetComponent<Rigidbody>();
-        if (rb == null) Debug.LogError("Rigidbody not found!");
+        previousPosition = transform.position;
+        previousVelocity = Vector3.zero;
+        previousRotation = transform.rotation;
     }
 
     void FixedUpdate()
     {
+        deltaTime = Time.fixedDeltaTime;
         orientationX = transform.rotation.eulerAngles.x;
         orientationY = transform.rotation.eulerAngles.y;
         orientationZ = transform.rotation.eulerAngles.z;
-        accelerationX = rb.velocity.x;
-        accelerationY = rb.velocity.y;
-        accelerationZ = rb.velocity.z;
-        gyroscopeX = rb.angularVelocity.x;
-        gyroscopeY = rb.angularVelocity.y;
-        gyroscopeZ = rb.angularVelocity.z;
+        Vector3 currentVelocity = (transform.position - previousPosition) / deltaTime;
+        Vector3 acceleration = (currentVelocity - previousVelocity) / deltaTime;
+        accelerationX = acceleration.x;
+        accelerationY = acceleration.y;
+        accelerationZ = acceleration.z;
+        Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(previousRotation);
+        Vector3 angularVelocity = new Vector3(
+            Mathf.DeltaAngle(0, deltaRotation.eulerAngles.x),
+            Mathf.DeltaAngle(0, deltaRotation.eulerAngles.y),
+            Mathf.DeltaAngle(0, deltaRotation.eulerAngles.z)
+        ) / deltaTime * Mathf.Deg2Rad;
+        gyroscopeX = angularVelocity.x;
+        gyroscopeY = angularVelocity.y;
+        gyroscopeZ = angularVelocity.z;
+        previousPosition = transform.position;
+        previousVelocity = currentVelocity;
+        previousRotation = transform.rotation;
     }
 }
